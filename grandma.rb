a283d19a2b636d1a6344b7e87cb1eb4cc280167d
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-25 13:44:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif  str == str.upcase
+  elsif  str == str.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
